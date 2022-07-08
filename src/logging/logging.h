ff3f61b871e2e87c496d427f9ff1a968be2fe5ef
EngineersBox <toastcraft.info@gmail.com>
2022-07-09 01:31:28 +1000

    Memory leak fixes (partial, still 891 bytes lost)

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -45,24 +45,22 @@ static inline char* logLevelToString(int level) {
 #define STDOUT stdout
 #define STDERR stderr
 
-//#define LOG(level, msg, ...) { \
-//    CHECK_TYPE(int, level); \
-//    if (level <= __min_log_level__) {   \
-//        time_t rawtime; \
-//        struct tm* timeinfo; \
-//        time(&rawtime); \
-//        timeinfo = localtime(&rawtime); \
-//        fprintf(level == LL_ERROR ? STDERR : STDOUT, "[%d/%d/%d %d:%d:%d] %s:%d [%s] :: " msg "\n", \
-//            timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, \
-//            timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, \
-//            __func__, __LINE__, \
-//            logLevelToString(level), \
-//            ##__VA_ARGS__ \
-//        ); \
-//    } \
-//}
-
-#define LOG(level, msg, ...) fprintf(level == LL_ERROR ? STDERR : STDOUT, msg "\n", ##__VA_ARGS__)
+#define LOG(level, msg, ...) { \
+    CHECK_TYPE(int, level); \
+    if (level <= __min_log_level__) {   \
+        time_t rawtime; \
+        struct tm* timeinfo; \
+        time(&rawtime); \
+        timeinfo = localtime(&rawtime); \
+        fprintf(level == LL_ERROR ? STDERR : STDOUT, "[%d/%d/%d %d:%d:%d] %s:%d [%s] :: " msg "\n", \
+            timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, \
+            timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, \
+            __func__, __LINE__, \
+            logLevelToString(level), \
+            ##__VA_ARGS__ \
+        ); \
+    } \
+}
 
 #define ERROR(msg, ...) (LOG(LL_ERROR, msg, __VA_ARGS__))
 #define WARN(msg, ...) (LOG(LL_WARN, msg, __VA_ARGS__))
