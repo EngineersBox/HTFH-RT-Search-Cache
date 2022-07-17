e8eb32891d4e9e70ecc70b0bebb9f322b1418f39
EngineersBox <toastcraft.info@gmail.com>
2022-07-17 23:44:04 +1000

    Added fatal logging level

--- src/logging/logging.h
+++ src/logging/logging.h
@@ -15,11 +15,12 @@ extern "C" {
 #include "../types/typecheck.h"
 
 enum LogLevel {
-    LL_ERROR = 0,
-    LL_WARN = 1,
-    LL_INFO = 2,
-    LL_DEBUG = 3,
-    LL_TRACE = 4
+    LL_FATAL = 0,
+    LL_ERROR = 1,
+    LL_WARN = 2,
+    LL_INFO = 3,
+    LL_DEBUG = 4,
+    LL_TRACE = 5
 };
 
 #ifndef ENABLE_LOGGING
@@ -73,7 +74,9 @@ extern __thread FILE* logFileHandle;
 #endif
 
 static inline char* logLevelToString(int level) {
-    if (level == LL_ERROR)  {
+    if (level == LL_FATAL) {
+        return "FATAL";
+    } else if (level == LL_ERROR)  {
         return "ERROR";
     } else if (level == LL_WARN) {
         return "WARN ";
@@ -132,30 +135,36 @@ static inline char* logLevelToString(int level) {
 //#define LOG(level, msg, ...) printf(msg "\n", ##__VA_ARGS__)
 
 #if MIN_LOG_LEVEL <= 0
+#define FATAL(msg, ...) LOG(LL_FATAL, msg, ##__VA_ARGS__); exit(1)
+#else
+#define FATAL(msg, ...) ({})
+#endif
+
+#if MIN_LOG_LEVEL <= 1
 #define ERROR(msg, ...) (LOG(LL_ERROR, msg, ##__VA_ARGS__))
 #else
 #define ERROR(msg, ...) ({})
 #endif
 
-#if MIN_LOG_LEVEL <= 1
+#if MIN_LOG_LEVEL <= 2
 #define WARN(msg, ...) (LOG(LL_WARN, msg, ##__VA_ARGS__))
 #else
 #define WARN(msg, ...) ({})
 #endif
 
-#if MIN_LOG_LEVEL <= 2
+#if MIN_LOG_LEVEL <= 3
 #define INFO(msg, ...) (LOG(LL_INFO, msg, ##__VA_ARGS__))
 #else
 #define INFO(msg, ...) ({})
 #endif
 
-#if MIN_LOG_LEVEL <= 3
+#if MIN_LOG_LEVEL <= 4
 #define DEBUG(msg, ...) (LOG(LL_DEBUG, msg, ##__VA_ARGS__))
 #else
 #define DEBUG(msg, ...) ({})
 #endif
 
-#if MIN_LOG_LEVEL <= 4
+#if MIN_LOG_LEVEL <= 5
 #define TRACE(msg, ...) (LOG(LL_TRACE, msg, ##__VA_ARGS__))
 #else
 #define TRACE(msg, ...) ({})
